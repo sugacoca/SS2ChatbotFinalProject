8dfd7cb430a3ca9333be15b8a044bd0b705d7ea3
Nghia Le <sanstheskeleton20112003@gmail.com>
2024-06-15 20:50:28 +0700

    finish

---
+++ StudyGPT.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Account (
+    email varchar(50),
+    password varchar(50)
+)
+CREATE TABLE `message` (
+    email varchar(50),
+    msg text,
+    role varchar(20),
+    time datetime
+)
